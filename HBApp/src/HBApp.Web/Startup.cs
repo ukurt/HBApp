b5dba4780c0f27a8055885fc4dfe220b61cd4b7d
Unal Kurt <dedde1907@hotmail.com>
2021-10-04 00:20:41 +0300

    fix

--- HBApp/src/HBApp.Web/Startup.cs
+++ HBApp/src/HBApp.Web/Startup.cs
@@ -69,10 +69,8 @@ namespace HBApp.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 
             app.UseEndpoints(endpoints =>

13bcf6a35c4cb8d9d4a151c919c86c59891d84d9
Unal Kurt <dedde1907@hotmail.com>
2021-10-04 00:16:51 +0300

    fix

--- HBApp/src/HBApp.Infrastructure/DefaultInfrastructureModule.cs
+++ HBApp/src/HBApp.Infrastructure/DefaultInfrastructureModule.cs
@@ -13,8 +13,6 @@ namespace HBApp.Infrastructure
     public class DefaultInfrastructureModule : Module
     {
         private readonly bool _isDevelopment = false;
-        private readonly List<Assembly> _assemblies = new List<Assembly>();
-
         public DefaultInfrastructureModule(bool isDevelopment, Assembly callingAssembly = null)
         {
             _isDevelopment = isDevelopment;

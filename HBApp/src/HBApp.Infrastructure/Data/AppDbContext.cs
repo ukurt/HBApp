fe4598d67011ad7cccc756333ed19b6dcdd9691b
Unal Kurt <dedde1907@hotmail.com>
2021-10-03 23:51:38 +0300

    delete mediatr

--- HBApp/src/HBApp.Infrastructure/Data/AppDbContext.cs
+++ HBApp/src/HBApp.Infrastructure/Data/AppDbContext.cs
@@ -30,26 +30,6 @@ namespace HBApp.Infrastructure.Data
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
             int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-            // ignore events if no dispatcher provided
-            if (_mediator == null) return result;
-
-            // dispatch events only if save was successful
-            var entitiesWithEvents = ChangeTracker.Entries<BaseEntity>()
-                .Select(e => e.Entity)
-                .Where(e => e.Events.Any())
-                .ToArray();
-
-            foreach (var entity in entitiesWithEvents)
-            {
-                var events = entity.Events.ToArray();
-                entity.Events.Clear();
-                foreach (var domainEvent in events)
-                {
-                    await _mediator.Publish(domainEvent).ConfigureAwait(false);
-                }
-            }
-
             return result;
         }
 
